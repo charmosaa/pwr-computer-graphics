eec212d0f530bea5bdf1f78986e43bd7cfc7b368
charmosaa <272682@student.pwr.edu.pl>
2025-03-06 22:32:50 +0100

    feat: task 3

--- Lab1/Lab3d.java
+++ Lab1/Lab3d.java
@@ -4,77 +4,93 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 
-public class Lab1b
+public class Lab3d
 {
     public static void main(String[] args)
     {
-        System.out.println("Grid pattern synthesis");
+        System.out.println("Pattern synthesis");
 
         BufferedImage image;
 
         // Image resolution
         int x_res, y_res;
 
-        // center coordinates
+        //center coordinates
         int x_c, y_c;
 
         // Loop variables - indices of the current row and column
         int i, j;
+        
+        //circle radious 
+        int r;
 
-        int grid_width;
-        int grid_x_distance;
-        int grid_y_distance;
+        //ring width
+        int w;
 
-        int color_grid;
-        int color_background;
-
+        //colors
+        int color_first, color_second;
 
         // Get required image resolution from command line arguments
         x_res = Integer.parseInt(args[0].trim());
         y_res = Integer.parseInt(args[1].trim());
 
-        // Get defined grid parameters
-        grid_width = Integer.parseInt(args[2].trim());
-        grid_x_distance = Integer.parseInt(args[3].trim()); 
-        grid_y_distance = Integer.parseInt(args[4].trim()); 
-        color_grid = int2RGB(Integer.parseInt(args[5].trim()),Integer.parseInt(args[6].trim()),Integer.parseInt(args[7].trim()));
-        color_background = int2RGB(Integer.parseInt(args[8].trim()),Integer.parseInt(args[9].trim()),Integer.parseInt(args[10].trim()));
+
+        // Get defined parameters
+        r = Integer.parseInt(args[2].trim()); 
+        w = Integer.parseInt(args[3].trim());
+        
+
+        color_first = int2RGB(0,0,0);
+        color_second = int2RGB(255,255,255);
 
         // Initialize an empty image, use pixel format
         // with RGB packed in the integer data type
         image = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
 
-        // Find coordinates of the image center
-        x_c = x_res / 2;
-        y_c = y_res / 2;
+         // Find coordinates of the image center
+         x_c = x_res / 2;
+         y_c = y_res / 2;
 
         // Process the image, pixel by pixel
         for (i = 0; i < y_res; i++)
             for (j = 0; j < x_res; j++)
             {
-                double dx;
-                double dy;
+                int dx,dy;
+                //grid center indexes
+                int gx,gy;
+
+                //distance from grid center
+                double dg;
+
+                int ring_index;
 
                 // Calculate distance to the image center
                 dx = Math.abs(j - x_c);
                 dy = Math.abs(i - y_c);
 
+                gx = dx % (2*r);
+                gy = dy % (2*r);
+
+                if (gx > r)
+                    gx = 2*r-gx;
+                if (gy > r)   
+                    gy = 2*r-gy;
+
+                dg = Math.sqrt(gx*gx + gy*gy);
+                ring_index = (int)dg / w;
+
                 // Make decision on the pixel color
-                // based on the ring index
-                if (dx % (grid_x_distance + grid_width) > grid_x_distance/2 && dx % (grid_x_distance + grid_width) <= grid_x_distance/2+grid_width)
-                    image.setRGB(j, i, color_grid);
-                else if (dy % (grid_y_distance + grid_width) > grid_y_distance/2 && dy % (grid_y_distance + grid_width) <= grid_y_distance/2+grid_width)
-                    image.setRGB(j, i, color_grid);
+                if (ring_index % 2 == 0)
+                    image.setRGB(j, i, color_first);
                 else
-                    // Odd ring - set white color
-                    image.setRGB(j, i, color_background);
+                    image.setRGB(j, i, color_second);
             }
 
         // Save the created image in a graphics file
         try
         {
-            ImageIO.write(image, "bmp", new File(args[11]));
-            System.out.println("Grid image created successfully");
+            ImageIO.write(image, "bmp", new File(args[4]));
+            System.out.println("Image created successfully");
         }
         catch (IOException e)
         {
@@ -95,3 +111,4 @@ public class Lab1b
         return (red << 16) + (green << 8) + blue;
     }
 }
+
