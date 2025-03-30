73dbc3c8e5b0b1730ae804d53f1249e191e3831a
charmosaa <272682@student.pwr.edu.pl>
2025-03-30 16:19:57 +0200

    feat: loading and saving image

--- Lab3/PaintDemo.java
+++ Lab3/PaintDemo.java
@@ -3,7 +3,13 @@ package Lab3;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class PaintDemo {
@@ -17,7 +23,7 @@ public class PaintDemo {
 }
 
 class DrawWndPane extends JPanel implements ActionListener, MouseListener, MouseMotionListener {
-    JButton button, applyColorButton;
+    JButton button, saveVector, loadVector, saveRaster;
     JRadioButton lineButton, rectButton, circleButton;
     ButtonGroup shapeGroup;
 
@@ -65,16 +71,26 @@ class DrawWndPane extends JPanel implements ActionListener, MouseListener, Mouse
         greenField.setBounds(470, 10, 40, 30);
         blueField.setBounds(520, 10, 40, 30);
 
-        applyColorButton = new JButton("Set Color");
-        applyColorButton.setBounds(420, 50, 140, 30);
+        saveVector = new JButton("Save txt");
+        loadVector = new JButton("Load");
+        saveRaster = new JButton("Save raster");
+
+        saveVector.setBounds(10, 50, 100, 30);
+        loadVector.setBounds(10, 90, 100, 30);
+        saveRaster.setBounds(10, 130, 100, 30);
+
+        saveVector.addActionListener(e -> saveVectorToFile());
+        saveRaster.addActionListener(e-> saveAsImage());
+        loadVector.addActionListener(e -> loadVectorFromFile());
 
         add(redField);
         add(greenField);
         add(blueField);
-        add(applyColorButton);
+        add(saveVector);
+        add(saveRaster);
+        add(loadVector);
 
         //applyColorButton.addActionListener(e -> updateShapeColor());
-
                 
         shapes = new ArrayList<>();
         
@@ -102,6 +118,83 @@ class DrawWndPane extends JPanel implements ActionListener, MouseListener, Mouse
 
         g2d.setPaintMode();
     }
+
+    private void loadVectorFromFile() {
+        shapes.clear();
+        shapeColors.clear();
+        
+        try (Scanner scanner = new Scanner(new File("Lab3/Assets/vector_image.txt"))) {
+            while (scanner.hasNext()) {
+                String type = scanner.next();
+                double x = scanner.nextDouble();
+                double y = scanner.nextDouble();
+                double w = scanner.nextDouble();
+                double h = scanner.nextDouble();
+                int r = scanner.nextInt();
+                int g = scanner.nextInt();
+                int b = scanner.nextInt();
+                Color color = new Color(r, g, b);
+    
+                if (type.equals("Line")) {
+                    shapes.add(new Line2D.Double(x, y, w, h));
+                } else if (type.equals("Rectangle")) {
+                    shapes.add(new Rectangle2D.Double(x, y, w, h));
+                } else if (type.equals("Circle")) {
+                    shapes.add(new Ellipse2D.Double(x, y, w, h));
+                }
+                shapeColors.add(color);
+            }
+            repaint();
+            JOptionPane.showMessageDialog(null, "Vector image loaded!");
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } 
+    }
+
+    private void saveVectorToFile() {
+        try (PrintWriter writer = new PrintWriter(new File("Lab3/Assets/vector_image.txt"))) {
+            for (int i = 0; i < shapes.size(); i++) {
+                Shape shape = shapes.get(i);
+                Color color = shapeColors.get(i);
+    
+                if (shape instanceof Line2D.Double line) {
+                    writer.printf("Line %.2f %.2f %.2f %.2f %d %d %d%n",
+                            line.x1, line.y1, line.x2, line.y2,
+                            color.getRed(), color.getGreen(), color.getBlue());
+                } else if (shape instanceof Rectangle2D.Double rect) {
+                    writer.printf("Rectangle %.2f %.2f %.2f %.2f %d %d %d%n",
+                            rect.x, rect.y, rect.width, rect.height,
+                            color.getRed(), color.getGreen(), color.getBlue());
+                } else if (shape instanceof Ellipse2D.Double circ) {
+                    writer.printf("Circle %.2f %.2f %.2f %.2f %d %d %d%n",
+                            circ.x, circ.y, circ.width, circ.height,
+                            color.getRed(), color.getGreen(), color.getBlue());
+                }
+            }
+            JOptionPane.showMessageDialog(null, "Vector image saved!");
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+    
+    private void saveAsImage() {
+        BufferedImage image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = image.createGraphics();
+        
+        paint(g2d);
+        g2d.dispose();
+    
+        try {
+            File file = new File("Lab3/Assets/image.png");
+            ImageIO.write(image, "PNG", file);
+            JOptionPane.showMessageDialog(null, "Image saved!");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+    
+
+    
 
     private boolean CatchClosePoint( double  x, double y )
    {
@@ -183,141 +276,87 @@ class DrawWndPane extends JPanel implements ActionListener, MouseListener, Mouse
        }
 	   return false;
    }
+ 
+    private void updateShapePosition(int x, int y) {
+        if (shapeCaught >= 0) {
+            Graphics g = getGraphics();
+            Graphics2D g2d = (Graphics2D) g;
     
-    private void UpdateLinePosition( int x, int y )
-    {
-        if ( shapeCaught >= 0 )
-        {	   
-           Graphics g = getGraphics();
-           Graphics2D  g2d = (Graphics2D)g;   	 
+            g2d.setXORMode(Color.white);
+            g2d.setColor(shapeColors.get(shapeCaught));
+    
+            Shape shape = shapes.get(shapeCaught);
+            g2d.draw(shape); // remove old shape
+    
+            // new shape position
+            if (shape instanceof Line2D.Double line) {
+                updateLine(line, x, y);
+            } else if (shape instanceof Rectangle2D.Double rect) {
+                updateRectangle(rect, x, y);
+            } else if (shape instanceof Ellipse2D.Double circ) {
+                updateCircle(circ, x, y);
+            }
+            
+            g2d.setColor(getCurrentColor());
+            shapeColors.set(shapeCaught, getCurrentColor());
+            g2d.draw(shape); 
+            g2d.setPaintMode();
+        }
+    }
+
+    private void updateLine(Line2D.Double line, int x, int y) {
+        switch (pointCaught) {
+            case 0 -> { line.x1 = x; line.y1 = y; }
+            case 1 -> { line.x2 = x; line.y2 = y; }
+            case 2 -> { // center - move 
+                double xc = x - (line.x1 + line.x2) / 2;
+                double yc = y - (line.y1 + line.y2) / 2;
+                line.x1 += xc;
+                line.y1 += yc;
+                line.x2 += xc;
+                line.y2 += yc;
+            }
+        }
+    }
+    private void updateRectangle(Rectangle2D.Double rect, int x, int y) {
+        switch (pointCaught) {
+            case 0 -> { // top-left corner
+                rect.width += (rect.x - x);
+                rect.height += (rect.y - y);
+                rect.x = x;
+                rect.y = y;
+            }
+            case 1 -> { // bottom-right corner
+                rect.width = Math.abs(rect.x - x);
+                rect.height = Math.abs(rect.y - y);
+                rect.x = Math.min(rect.x, x);
+                rect.y = Math.min(rect.y, y);
+            }
+            case 2 -> { // center - moving 
+                Point2D center = getShapeCenter(rect);
+                rect.x += (x - center.getX());
+                rect.y += (y - center.getY());
+            }
+        }
+    }
+    
+    private void updateCircle(Ellipse2D.Double circ, int x, int y) {
+        Point2D center = getShapeCenter(circ);
+        switch (pointCaught) {
+            case 0 -> { // center - moving
+                circ.x += (x - center.getX());
+                circ.y += (y - center.getY());
+            }
+            case 1 -> { // on the perimeter - resize 
+                double radius = center.distance(x, y);
+                circ.width = circ.height = 2 * radius;
+                circ.x = center.getX() - radius;
+                circ.y = center.getY() - radius;
+            }
+        }
+    }
         
-           g2d.setXORMode( getBackground() );
-           g2d.setColor(shapeColors.get(shapeCaught));
- 
-            Line2D.Double  line;
-            line = (Line2D.Double) shapes.get( shapeCaught );
-           
-           // Delete at previous position          
-           g2d.draw( line );  
- 
-            // Update line end position
-            switch (pointCaught) {
-                case 0:
-                    line.x1 = x;
-                    line.y1 = y;
-                    break;
-                case 1:
-                    line.x2 = x;
-                    line.y2 = y;
-                    break;
-                case 2:
-                    double xc = x - 0.5 * (line.x1 + line.x2);
-                    double yc = y - 0.5 * (line.y1 + line.y2);           	  
-                    line.x1 += xc;
-                    line.y1 += yc;
-                    line.x2 += xc;
-                    line.y2 += yc;
-                    break;  
-                default:
-                    break;
-            }
-           
-           // Draw line at new position          
-           g2d.draw( line );
-           g2d.setPaintMode();                   
-        }   
-    }
-
-    private void UpdateRectPosition( int x, int y )
-    {
-        if ( shapeCaught >= 0 )
-        {	   
-            Graphics g = getGraphics();
-            Graphics2D  g2d = (Graphics2D)g;   	 
-            
-            //g2d.setXORMode( getBackground() );
-            g2d.setXORMode( new Color( 255,255,255) );  
-            g2d.setColor(shapeColors.get(shapeCaught));
-            //g2d.setXORMode( Color.red );           
-
-            Rectangle2D.Double  rect;
-            rect = (Rectangle2D.Double) shapes.get( shapeCaught );
-            
-            // Delete at previous position          
-            g2d.draw( rect );  
- 
-            // Update line end position
-            switch (pointCaught) {
-                case 0: //left top
-                    rect.width += (rect.x - x);
-                    rect.height += (rect.y - y);
-                    rect.x = x;
-                    rect.y = y;
-                    break;
-                case 1: // right bottom
-                    rect.width = Math.abs(rect.x - x);
-                    rect.height = Math.abs(rect.y - y);
-                    rect.x = Math.min (rect.x, x);
-                    rect.y = Math.min (rect.y, y);
-                    break;
-                case 2:
-                    Point2D center = getShapeCenter(rect);
-                    double xc = x - center.getX();
-                    double yc = y - center.getY();           	  
-                    rect.x += xc;
-                    rect.y += yc;
-                    break;  
-                default:
-                    break;
-            }
-           
-           // Draw line at new position          
-           g2d.draw( rect );
-           g2d.setPaintMode();                   
-        }   
-    }
-
     
-    private void UpdateCriclePosition( int x, int y )
-    {
-        if ( shapeCaught >= 0 )
-        {	   
-            Graphics g = getGraphics();
-            Graphics2D  g2d = (Graphics2D)g;   	 
-            
-            g2d.setXORMode( new Color( 255,255,255) );       
-            g2d.setColor(shapeColors.get(shapeCaught));
-
-            Ellipse2D.Double  circ;
-            circ = (Ellipse2D.Double) shapes.get( shapeCaught );
-            
-            // Delete at previous position          
-            g2d.draw( circ );
- 
-            Point2D center = getShapeCenter(circ);
-            // Update line end position
-            switch (pointCaught) {
-                case 0: // middle
-                    circ.x += (x-center.getX());
-                    circ.y += (y - center.getY());
-                    break;
-                case 1: // on the perimeter 
-                    double radious = center.distance(x, y);
-
-                    circ.width = 2*radious;
-                    circ.height = 2*radious;
-                    circ.x = center.getX() - radious;
-                    circ.y = center.getY() - radious;
-                    break; 
-                default:
-                    break;
-            }
-           
-           // Draw line at new position          
-           g2d.draw( circ );
-           g2d.setPaintMode();                   
-        }   
-    }
 
     private Color getCurrentColor() {
         try {
@@ -380,21 +419,12 @@ class DrawWndPane extends JPanel implements ActionListener, MouseListener, Mouse
                     removeShape();
                     shapes.remove(i);
                     shapeColors.remove(i);
-                    //repaint();
                     return;
                 }
             }
         } else {
             if (CatchClosePoint(e.getX(),e.getY()))
-            {
-            switch(currentShape){
-                    case "Line" -> UpdateLinePosition(e.getX(),e.getY());
-                    case "Rectangle" -> UpdateRectPosition(e.getX(),e.getY());
-                    case "Circle" -> UpdateCriclePosition(e.getX(),e.getY());
-                    default -> {
-                    }
-                }
-            }
+                updateShapePosition(e.getX(), e.getY());
                 
             else{
                 switch (currentShape) {
@@ -414,16 +444,7 @@ class DrawWndPane extends JPanel implements ActionListener, MouseListener, Mouse
     
     public void mouseDragged(MouseEvent e) {
         if (shapeCaught >= 0) {
-            Shape shape = shapes.get(shapeCaught);
-            if (shape instanceof Line2D line2D) {
-                UpdateLinePosition(e.getX(), e.getY());
-            } else if (shape instanceof Rectangle2D rect) {
-                //rect.setFrameFromDiagonal(rect.getX(), rect.getY(), e.getX(), e.getY());
-                UpdateRectPosition(e.getX(), e.getY());
-            } else if (shape instanceof Ellipse2D ellipse) {
-                UpdateCriclePosition(e.getX(), e.getY());
-            }
-            repaint();
+            updateShapePosition(e.getX(), e.getY());
         }
     }
     
@@ -443,6 +464,8 @@ class SmpWindow extends JFrame {
         contents.add(new DrawWndPane());
     }
 }
+
+
 
 
 
